2e96e36b2e68a2be3f20b2fe9511aefec809697d
Fran Asir <Fran Asir>
2016-10-13 12:21:01 +0200

    0982.c Añadido campo, remasterización y movimiento del sol

--- myhouse/house-master/Picture.java
+++ myhouse/house-master/Picture.java
@@ -13,9 +13,10 @@ public class Picture
 {
     private Square wall;
     private Square window;
+    private Square field;
     private Triangle roof;
     private Circle sun;
-    private Circle sun2;
+    
 
     /**
      * Constructor for objects of class Picture
@@ -50,18 +51,20 @@ public class Picture
         roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("blue");
+        sun.changeColor("yellow");
         sun.moveHorizontal(100);
-        sun.moveVertical(-40);
         sun.changeSize(80);
-        sun.makeVisible();
         
-        sun2 = new Circle();
-        sun2.changeColor("green");
-        sun2.moveHorizontal(200);
-        sun2.moveVertical(-30);
-        sun2.changeSize(80);
-        sun2.makeVisible();
+        window = new Square();
+        window.changeColor("green");
+        window.moveHorizontal(-310);
+        window.moveVertical(140);
+        window.changeSize(1000);
+        window.makeVisible();
+        
+        sun.makeVisible();
+        sun.slowMoveVertical(90);
+        sun.slowMoveVertical(-100);
     }
 
     /**
